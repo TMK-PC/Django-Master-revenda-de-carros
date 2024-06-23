0dc9f514f413a898222399c5b8841f46c4cc296a
Tobias <tobiasmichaelsenklein@gmail.com>
2024-06-22 23:21:16 -0300

    allowe host config

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+0*29n$gn&$=&$e-*hbm($ud=mm8jtcnn88mvkzxz9r*gm0cht
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
